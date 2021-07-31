d481e5dcfe6136a00406253eba92349504001f81
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2021-07-31 18:01:40 +0800

    new db

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd8teipp1gmlg5v',
+        'USER': 'dwdttgdfimgxow',
+        'PASSWORD': 'd90eb831fbbf54fc7246b7a53a959049c5bffb9c25730fc60e5977aa7e3bb72e',
+        'HOST': 'ec2-34-206-8-52.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -136,5 +146,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['healthcare-on-demand.herokuapp.com/', '127.0.0.1']
 CORS_ORIGIN_ALLOW_ALL = True
